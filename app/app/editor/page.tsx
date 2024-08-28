3b4e400c32fe2d1966b1a275e9a91c8fa1f25126
astro-develops <astromp777@gmail.com>
2024-08-27 20:09:00 -0400

    done

--- app/app/editor/page.tsx
+++ app/app/editor/page.tsx
@@ -3,11 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    This is Home
-    <br/>    
-    <br/>
-    <br/>
-    <a href="/editor">Go to Editor</a>
+    <a href="./editor">This is the editor</a>
     </>
   );
 }
